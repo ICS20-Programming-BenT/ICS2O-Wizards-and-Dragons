d0fec9081016f4a10ab2a70e2e7d043f389ebb6e
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-05-30 15:20:52 +0000

    Added comments to code and added favicon

--- js/game.js
+++ js/game.js
@@ -1,10 +1,29 @@
 /* global Phaser */
 
+// Copyright (c) 2023 Ben Thomson. All rights reserved.
+//
+// Created by: Ben Thomson
+// Created on: 05/30/2023
+// This is the Phaser3 configuration file
+
+//* Game scene */
 const  config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true
+    }
+  },
+  // Set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // We place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
