d26b40bd4e308eb2b915e5933c488f7f9e257449
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-06-07 00:07:43 +0000

    Finished tutorials!

--- js/gameScene.js
+++ js/gameScene.js
@@ -8,6 +8,31 @@
 
 // The code written below adds to the code already in Phaser.Scene
 class GameScene extends Phaser.Scene {
+
+  // Create a dragon
+  createDragon() {
+    // Creating enemies at a random x location between 1 and 1920 px
+    const dragonXLocation = Math.floor(Math.random() * 1920) +1
+
+    // Using a variable and Math.random() to make the dragons less predictable
+    let dragonXVelocity = Math.floor(Math.random() * 50) + 1
+
+    // Making the dragons move slightly up or down
+    dragonXVelocity *= Math.round(Math.random()) ? 1 : -1
+
+    // Creating a variable that makes a dragon appear each time this function is called
+    const aDragon = this.physics.add.sprite(dragonXLocation, -100, "dragon").setScale(0.20)
+
+    // Adding an y velocity to the dragon
+    aDragon.body.velocity.y = 200
+
+    // Adding the x velocity using the variable defined above
+    aDragon.body.velocity.x = dragonXVelocity
+
+    // Adding the new dragon created to the dragon group
+    this.dragonGroup.add(aDragon)
+  }
+  
   constructor () {
 
     // Using the "gameScene" key to create an object
@@ -21,6 +46,13 @@ class GameScene extends Phaser.Scene {
 
     // Initializing the fire missile variable as false
     this.fireMissile = false
+
+    this.score = 0
+    this.scoreText = null
+    this.scoreTextStyle = { font: "65px Georgia", fill: "#48bfe3", align: "center"}
+
+    this.gameOverText = null
+    this.gameOverTextStyle = { font: "65px Georgia", fill: "#5a189a", align: "center"}
   }
 
   init (data) {
@@ -32,10 +64,16 @@ class GameScene extends Phaser.Scene {
     // Places Game Scene in the console to let programmer know the scene is being displayed
     console.log("Game Scene")
 
-    // Giving Phaser the images for the background, the wizard and the missile
+    // Giving Phaser the images for the background, the wizard, the missile and the dragon 
     this.load.image("mountainBackground", "images/gameSceneImage.png")
     this.load.image("wizard", "images/wizard.png")
     this.load.image("missile", "images/magicMissile.png")
+    this.load.image("dragon", "images/dragon.png")
+
+    // Loading the various sound effects for the game
+    this.load.audio("spell", "./sounds/spell-sound-effect.wav")
+    this.load.audio("explosion", "./sounds/explosion-sound-effect.wav")
+    this.load.audio("gameOver", "./sounds/game-over.wav")
   }
 
   create (data) {
@@ -43,11 +81,58 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, "mountainBackground").setScale(1.0)
     this.background.setOrigin(0, 0)
 
+    // Displaying styled score text
+    this.scoreText = this.add.text(10, 10, "Score: " + this.score.toString(), this.scoreTextStyle)
+
     // Creating the wizard sprite on the screen
     this.wizard = this.physics.add.sprite(1920 / 2, 1080 - 100, "wizard").setScale(0.35)
 
     // Creating a group for the same code to apply to all the missiles
     this.missileGroup = this.physics.add.group()
+
+    // Creating a group for the same code to apply to all the dragons
+    this.dragonGroup = this.add.group()
+    this.createDragon()
+
+    // Adding a physics collider so that when missiles hit dragons, a function is called
+    this.physics.add.collider(this.missileGroup, this.dragonGroup, function (missileCollide, dragonCollide) {
+      // Destroying the dragon upon collision
+      dragonCollide.destroy()
+      
+      // Destroying the missile upon collision
+      missileCollide.destroy()
+
+      // Playing the explosion sound effect
+      this.sound.play("explosion")
+
+       // Updating score upon explosion of dragon
+      this.score += 1
+      this.scoreText.setText("Score: " + this.score.toString())
+
+      // Recreating two new dragons for each that is destroyed
+      this.createDragon()
+      this.createDragon()
+
+      // Binding above code to the class created at the top of the file
+    }.bind(this))
+
+    // Adding a physics collider so that when a dragon hits the wizard, a function is called
+    this.physics.add.collider(this.wizard, this.dragonGroup, function (wizardCollide, dragonCollide) {
+      // Playing the game over sound effect
+      this.sound.play("gameOver")
+
+      // Pausing the physics
+      this.physics.pause()
+
+      // Destroying both the dragons and the wizard
+      dragonCollide.destroy()
+      wizardCollide.destroy()
+
+      // Displaying the game over text and allowing the user to click on it to start over
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5)
+      this.gameOverText.setInteractive({ useHandCursor: true})
+      this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
+    }.bind(this))
   }
 
   update (time, delta) {
@@ -89,6 +174,9 @@ class GameScene extends Phaser.Scene {
 
         // Adding the new missile to the group of missiles in the "create" section
         this.missileGroup.add(aNewMissile)
+
+        // Playing the sound of the spell being fired
+        this.sound.play("spell")
       }
     }
 
@@ -96,6 +184,14 @@ class GameScene extends Phaser.Scene {
     if (keySpaceObj.isUp === true) {
       this.fireMissile = false
     }
+
+    // To make missiles move up the screen
+    this.missileGroup.children.each(function (item) {
+      item.y -= 15
+      if (item.y < 0) {
+        item.destroy()
+      }
+    })
   }
 }
 
